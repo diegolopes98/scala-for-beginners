46b209b7abf8fa960846b888f20f99cce95bb64b
Diego Lopes <diegoplopes5@gmail.com>
2021-03-01 11:02:54 -0300

    feat: add class examples

--- src/lectures/oop/OOBasics.scala
+++ src/lectures/oop/OOBasics.scala
@@ -22,3 +22,46 @@ class Person(name: String, val age: Int) {
 }
 
 // to convert class parameter to field should put val or var before
+
+/*
+* Novel and Write
+*
+* Writer: first name, surname, year
+* - method: fullname
+*
+* Novel: name, release year, author
+* - authorAge
+* - isWrittenBy(author)
+* - copy (new year of release) = new instance of novel
+* */
+
+class Writer(firstName: String, surname: String, val year: Int) {
+  def fullName(): String = s"$firstName $surname"
+}
+
+class Novel(name: String, releaseYear: Int, author: Writer) {
+  def authorAge(): Int = 2021 - author.year
+  def isWrittenBy(author: Writer): Boolean = this.author == author
+  def copy(year: Int): Novel = new Novel(name, year, author)
+}
+
+/*
+  Counter class
+    - receives an int value
+    - method current count
+    - method to increment/decrement => new Counter
+    - overload inc/dec to receive an amount
+ */
+
+class Counter(val cur: Int = 0) {
+  def inc(): Counter = new Counter(cur + 1)
+  def dec(): Counter = new Counter(cur - 1)
+
+  def inc(amount:Int): Counter =
+    if (amount <= 0 ) this
+    else inc.inc(amount - 1)
+
+  def dec(amount:Int): Counter =
+    if (amount <= 0 ) this
+    else dec.dec(amount - 1)
+}
