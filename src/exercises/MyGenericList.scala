0b0564ea9fecbc66f76e5ffc3df47accfed4513a
Diego Lopes <diegoplopes5@gmail.com>
2021-03-01 18:18:42 -0300

    feat: add generic linked list exercise

--- src/exercises/MyGenericList.scala
+++ src/exercises/MyGenericList.scala
@@ -1,5 +1,38 @@
 package exercises
 
-class MyGenericList {
+abstract class MyGenericList[+A] {
+  def head(): A
+  def tail(): MyGenericList[A]
+  def isEmpty(): Boolean
+  def add[ B >: A ](elem: B): MyGenericList[B]
+  def printElements(): String
+  override def toString(): String = "[" + printElements + "]"
+}
 
+object GEmpty extends MyGenericList[Nothing] {
+  def head(): Nothing = throw new NoSuchElementException
+  def tail(): MyGenericList[Nothing] = throw new NoSuchElementException
+  def isEmpty(): Boolean = true
+  def add[B >: Nothing](elem: B): MyGenericList[B] = new GCons(elem, GEmpty)
+
+  override def printElements(): String = ""
+
+}
+
+class GCons[+A](h: A, t: MyGenericList[A]) extends MyGenericList[A] {
+  def head(): A = h
+  def tail(): MyGenericList[A] = t
+  def isEmpty(): Boolean = false
+  def add[ B >: A ](elem: B): MyGenericList[B] = new GCons(elem, this)
+
+  override def printElements(): String =
+    if(t.isEmpty()) s"$h"
+    else s"$h, ${t.printElements()}"
+}
+
+object GenericListTest extends App {
+  val listInt: MyGenericList[Int] = new GCons(1, new GCons(2, new GCons(3, GEmpty)))
+  val listStr: MyGenericList[String] = new GCons("Hello", new GCons("Scala", GEmpty))
+  println(listInt.toString())
+  println(listStr.toString())
 }
