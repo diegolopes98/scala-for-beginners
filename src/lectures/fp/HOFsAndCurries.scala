6c1f47411c8b92a28a24e0b3bd98240efa1485b6
Diego Lopes <diegoplopes5@gmail.com>
2021-03-02 15:50:40 -0300

    feat: add hofs and curries example

---
+++ src/lectures/fp/HOFsAndCurries.scala
@@ -0,0 +1,42 @@
+package lectures.fp
+
+object HOFsAndCurries extends App {
+
+   val superFunction: (Int, (String, (Int => Boolean)) => Int) => (Int => Int) = null
+  // higher order function (hof)
+
+  // fn that applies a function n times over a value x
+  // nTimes(f, n, x)
+
+  def nTimes(f: Int => Int, n: Int, x: Int): Int =
+    if (n <= 0) x
+    else nTimes(f, n - 1, f(x))
+
+  val plusOne: Int => Int = _ + 1
+  println(nTimes(plusOne, 10, 1))
+
+  def nTimesBetter(f: Int => Int, n: Int): Int => Int =
+    if (n <= 0) (x: Int) => x
+    else (x: Int) => nTimesBetter(f, n - 1)(f(x))
+
+//  def nTimesBetter2(f: Int => Int, n: Int): Int => Int =
+//    if (n <= 0) (x: Int) => x
+//    else (x: Int) => nTimesBetter(f, n - 1)(f(x))
+
+  val plus10 = nTimesBetter(plusOne, 10)
+  println(plus10 (1))
+
+  // curried functions
+  val superAdder: Int => (Int => Int) = (x: Int) => (y: Int) => x + y
+  val add3 = superAdder(3)
+  println(add3(10))
+  println(superAdder(3)(10))
+
+  // functions with multiple parameter lists
+  def curriedFormatter(c: String)(x: Double): String = c.format(x)
+  val standardFormat: (Double => String) = curriedFormatter("%4.2f")
+  val preciseFormat: (Double => String) = curriedFormatter("%10.8f")
+
+  println(standardFormat(Math.PI))
+  println(preciseFormat(Math.PI))
+}
